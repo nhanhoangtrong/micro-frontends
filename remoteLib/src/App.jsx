7116d7bffff4ebbfb8898301735684ce56e07e4b
Nhan Hoang Trong <nhan.hoang2@tiki.vn>
2021-10-22 11:25:14 +0700

    initialize

---
+++ remoteLib/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SmartButton from './SmartButton';
+
+const App = () => {
+  return (
+    <div>
+      <p>This is App for remote lib example</p>
+      <SmartButton>Hello world</SmartButton>
+    </div>
+  );
+};
+
+export default App;
---
+++ remoteLib/src/SmartButton.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SmartButton({ children }) {
+  return <button className="">{children}</button>;
+}
+
+SmartButton.propTypes = {};
+
+export default SmartButton;
